7b19bb8174b7981d29eadbc881d68a329e1b9d61
Daksh <Dakshkanotra@gmail.com>
2020-11-22 20:40:05 +0530

    This should make writing code faster.

---
+++ template.cpp
@@ -0,0 +1,32 @@
+//How can someone forget these
+# include <bits/stdc++.h>
+using namespace std;
+
+// Define some commonly used datatypes
+# define ll long long
+# define ld long double
+
+// For loop
+# define flp(i, n) for (int i = 0; i < n; i++)
+# define flpll(i, n) for (ll i = 0; i < n; i++)
+
+// Vector stuff
+// Initialize vector
+# define vi vector<int>
+# define vll vector<ll>
+# define vpi vector <pair<int, int> >
+# define vpll vector <pair <ll, ll> >
+
+// Operations
+# define pb push_back
+# define first f
+# define second s
+
+// Test input
+# define TEST int t;\
+         cin >> t;\
+         while (t--)
+
+int main() {
+    return 0;
+}
